bb524b51261a1df5cde828abcd79eb8e83f84ca0
Brian Temu <bryangasper2124@gmail.com>
2023-12-02 20:05:01 -0500

    ADD: not header supportd

--- pages/GoogleAnalytics.tsx
+++ pages/GoogleAnalytics.tsx
@@ -1,11 +1,10 @@
 import Script from "next/script";
 
-const GoogleAnalytics = ({ ga_id }: { ga_id: string }) => (
+const GoogleAnalytics = ({ ga_id }:{ ga_id: string }) => (
   <>
     <Script
       async
-      src={`https://www.googletagmanager.com/gtag/js? 
-      id=${ga_id}`}
+      src={`https://www.googletagmanager.com/gtag/js?id=${ga_id}`}
     ></Script>
     <Script
       id="google-analytics"
