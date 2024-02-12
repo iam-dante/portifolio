d2a1bb341c019aed8875fb0d1424a3d72f82c35d
Brian Temu <bryangasper2124@gmail.com>
2024-02-11 22:21:53 -0500

    FIX: removed google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,10 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Analytics } from "@vercel/analytics/react";
-// import GoogleAnalytics from "./GoogleAnalytics";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      {/* <GoogleAnalytics ga_id={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS} /> */}
       <Component {...pageProps} />
       <Analytics />
     </>
