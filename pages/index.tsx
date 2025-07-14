90880c8d6f0f2fc12538c4fe143ae9bce2ad8667
iam-dante <bryangasper2124@gmail.com>
2025-07-13 20:33:15 -0400

    resume

--- pages/index.tsx
+++ pages/index.tsx
@@ -121,7 +121,7 @@ const Home = (): JSX.Element => {
                 Projects
               </button>
               <Link
-                href="https://drive.google.com/file/d/1xB1cX8aRRtSrQwExlasPljDIJqpSBCEM/view?usp=sharing"
+                href="https://drive.google.com/file/d/1iIaWRLowxjojHeIOv2_HLmSrbiu5-mlG/view?usp=sharing"
                 className="text-gray-600 hover:text-black dark:text-gray-300 dark:hover:text-white hover:border-b-2 hover:border-sky-600  py-2"
                 target="_blank"
               >
