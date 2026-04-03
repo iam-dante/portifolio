dcf9a17d0e092b0e2fce3cd8104e4b7b14ba58f3
iam-dante <bryangasper2124@gmail.com>
2026-04-03 07:05:39 -0600

    ADD:new resume

--- pages/index.tsx
+++ pages/index.tsx
@@ -126,7 +126,7 @@ const Home = (): React.ReactElement => {
                 Projects
               </button>
               <Link
-                href="https://drive.google.com/file/d/1GNIRtsEIIMmRFCVcAsgN5wwHjadqetcO/view?usp=sharing"
+                href="https://drive.google.com/file/d/1K_EUYt16geFc2M9BqjJQOJZrfPfBIu9N/view?usp=sharing"
                 className="text-gray-600 hover:text-black dark:text-gray-300 dark:hover:text-white hover:border-b-2 hover:border-sky-600  py-2"
                 target="_blank"
               >
